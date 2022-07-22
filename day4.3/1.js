a6ae497e6ec061a1f807745d90618caf1ab2c74c
jaidernunes <jaidernf@gmail.com>
2022-07-22 14:33:29 -0300

    Logic Academy - Day 4.3 - JS

---
+++ day4.3/1.js
@@ -0,0 +1,8 @@
+const numbers = [2, 5, 3, 7, 22, 44, 55, 51, 16, 27]
+let valor = 27;
+
+if (numbers.includes(valor)){
+        console.log(numbers.indexOf(valor))
+    } else {
+        console.log('Elemento não encontrado no array')
+    }
